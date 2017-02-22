f733dca4ab4b1f2266abe38b1b88f7c8d0409335
Andrew <twintrool@gmail.com>
2017-02-21 21:04:02 -0700

    cursor and ui
    cursor moves arounf alot better but is not perfect. Added buttons to the
    main screen for control. There are not interactable yet. Time is
    displayed in the corner too.

---
+++ Assets/KinectView/Scripts/Timer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+    Text text;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = System.DateTime.Now.ToString("HH:mm");
+    }
+}
--- Assets/Test2/Interaction.cs
+++ Assets/Test2/Interaction.cs
@@ -12,6 +12,7 @@ public class Interaction : MonoBehaviour
     public JointType TrackedRightHand;
     public JointType TrackedRightThumb;
     public JointType TrackedRightTip;
+    public JointType TrackedRightElbow;
     private BodySourceManager bodyManager;
     private Body[] bodies;
 
@@ -40,47 +41,67 @@ public class Interaction : MonoBehaviour
             return;
         }
         bodies = bodyManager.GetData();
+
         if (bodies == null)
         {
             return;
         }
+
         foreach (var body in bodies)
         {
             if (body == null)
             {
                 continue;
             }
+
             if (body.IsTracked)
             {
-
                 var posRight = body.Joints[TrackedRight].Position;
                 var posRH = body.Joints[TrackedRightHand].Position;
                 var posRT = body.Joints[TrackedRightThumb].Position;
                 var posRTip = body.Joints[TrackedRightTip].Position;
-               //  if (posRH.X < posRight.X )
-                  {
-                      gameObject.transform.position = new Vector3((posRH.X * 10f) + 510, (posRH.Y * 20f) + 960);
+                var posRElbow = body.Joints[TrackedRightElbow].Position;
 
-                  }/*
+                if (posRH.Y > posRElbow.Y) //hand is right hand is open
+                {
+                    Debug.Log("I see you " + posRH.Y + " " + posRElbow.Y);
+                    gameObject.transform.position =
+                        new Vector3((posRH.X * 1800f) + 510, (posRH.Y * 2500f) + 540);
+                    //gameObject.transform.position =
+                    //new Vector3(0, 0);
+                    // Draw Hand Cursor
+
+                }
+                else
+                {
+                    gameObject.transform.position =
+                      new Vector3(1100, 0);
+                }
+                /*
                 //  else
                   {
                       //gameObject.transform.position = new Vector3(posRH.X, posRight.X);
                       gameObject.transform.position = new Vector3(-4, -4);
 
                   }*/
-                 /*
-                if (posRight.X < posRH.X)
-                {
-                    while (posRight.X - posRH.X < 0.2)
-                    {
-                        gameObject.transform.position = new Vector3(posRH.X, posRight.X);
-                    }
-                }
-                else
-                {
-                    gameObject.transform.position = new Vector3(-4, -4);
-                    
-                }*/
+                /*
+               if (posRight.X < posRH.X)
+               {
+                   while (posRight.X - posRH.X < 0.2)
+                   {
+                       gameObject.transform.position = new Vector3(posRH.X, posRight.X);
+                   }
+               }
+               else
+               {
+                   gameObject.transform.position = new Vector3(-4, -4);
+
+               }*/
+            }
+            else
+            {
+                gameObject.transform.position =
+                     new Vector3(1100, 0);
             }
         }
 
