8a28a708b1d960606170969b2e873a01c2605a00
Gabi Staheli <Gabi Staheli>
2017-03-02 08:49:43 -0700

    Added Scollbar For Viewing Different Objects

---
+++ Assets/Move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float max;
+    public float min;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < max)
+        {
+            transform.position += Vector3.right * Time.deltaTime * 400f;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > min)
+        {
+            transform.position += Vector3.left * Time.deltaTime * 400f;
+        }
+    }
+}
